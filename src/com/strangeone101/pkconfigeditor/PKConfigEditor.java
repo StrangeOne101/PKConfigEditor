cd285e3419637a8affc59a9d51312c9efd9c9e64
StrangeOne101 <strange.toby@gmail.@gmail.com>
2016-08-15 07:04:38 +1200

    Init Commit

---
+++ src/com/strangeone101/pkconfigeditor/PKConfigEditor.java
@@ -0,0 +1,21 @@
+package com.strangeone101.pkconfigeditor;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PKConfigEditor extends JavaPlugin
+{
+
+	public static PKConfigEditor plugin;
+	
+	@Override
+	public void onEnable() {
+		super.onEnable();
+		
+		plugin = this;
+		
+		new ConfigCommand();
+		
+		this.getLogger().info("PKConfigEditor loaded!");
+	}
+
+}
